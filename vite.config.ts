93c67b93a5f97daba8a6d5c2f72c9aa99ad5388f
thblees <tblees@gmx.com>
2026-03-31 10:35:37 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/masterclass_landing/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
